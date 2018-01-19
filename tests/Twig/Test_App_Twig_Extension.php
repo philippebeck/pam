ee02e1886549681302ae7518706a2d9abf283525
Philippe Beck <philippe@philippebeck.net>
2018-01-19 04:54:02 +0100

    Adding the Test App Twig Extension...

---
+++ tests/Twig/Test_App_Twig_Extension.php
@@ -0,0 +1,15 @@
+<?php
+
+/* *********************************** */
+/* ***** TEST APP TWIG EXTENSION ***** */
+/* *********************************** */
+
+namespace Tests\Twig;
+
+use PHPUnit\Pam\TestCase;
+use App\Helper\App_Twig_Extension;
+
+/**
+ * Tests the Twig extension methods
+ */
+class Test_App_Twig_Extension extends TestCase {}
