8adbbea17ecbe0665e98663dc88217c3e31f0cac
Philippe Beck <philippe@philippebeck.net>
2018-01-19 04:52:01 +0100

    Adding the Home Model...

---
+++ src/Model/HomeModel.php
@@ -0,0 +1,15 @@
+<?php
+
+// ********************** \\
+// ***** HOME MODEL ***** \\
+// ********************** \\
+
+namespace App\Model;
+
+use Pam\Model\Model;
+
+
+/** ****************\
+ * CRUD for home
+ */
+class HomeModel extends Model {}
