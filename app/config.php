bf209de2b9dac652bb24d3f83a7fed124b4226fc
Philippe Beck <philippe@philippebeck.net>
2018-01-19 04:33:39 +0100

    Adding the DB config file...

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// ********************** \\
+// ***** CONFIG APP ***** \\
+// ********************** \\
+
+
+/** ******************************************************\
+ * Defines the global constants for access to the database
+ */
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'pam');
+define('DB_USER', 'root');
+define('DB_PASS', '');
