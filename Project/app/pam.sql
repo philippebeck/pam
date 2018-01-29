8c278f8839072532db88bcc762bf2359aa92457a
Philippe Beck <philippe@philippebeck.net>
2018-01-29 13:55:03 +0100

    Adding the user empty stylesheet, the user empty script file, the pam logo & the sql basic file

---
+++ Project/app/pam.sql
@@ -0,0 +1,20 @@
+-- ************************
+-- ***** DATABASE PAM *****
+-- ************************
+
+
+
+-- !! This is a database example, allowing to start the Framework...
+
+
+
+-- Drops the old database before creates a new one
+DROP DATABASE IF EXISTS pam;
+
+
+-- Creates the database
+CREATE DATABASE pam CHARACTER SET 'utf8';
+
+
+-- Uses the database
+USE pam;
---
+++ Project/htdocs/js/script.js
@@ -0,0 +1,18 @@
+// ****************** \\
+// ***** SCRIPT ***** \\
+// ****************** \\
+
+
+
+// Strict mode
+'use strict';
+
+
+
+// Waits the complete page loading to launch the script
+document.addEventListener('DOMContentLoaded', function(e)
+{
+  // Put your project script here
+
+  }
+);
