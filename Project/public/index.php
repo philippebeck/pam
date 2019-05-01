f7ff151c7b91a60a085934be54d4a1f858c15ff1
Philippe Beck <philippe@philippebeck.net>
2019-05-01 16:53:48 +0200

    Add tracy profiler

--- Project/public/index.php
+++ Project/public/index.php
@@ -5,20 +5,18 @@
 // ***************** \\
 
 use Pam\Controller\FrontController;
-
+use Tracy\Debugger;
 
 // Loads Composer autoload
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-
 // Creates a front controller instance
 $frontController = new FrontController();
 
-
 // Basic tests area
+Debugger::enable();
 // print_r($_SESSION);
 // var_dump($frontController);
-
 
 // Calls the run method on the front controller instance
 $frontController->run();
