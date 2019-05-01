63a87f83a8b36a17cdf49e4fbd8960f6c06846f6
Philippe Beck <philippe@philippebeck.net>
2019-05-01 16:01:37 +0200

    Fix the ControllerInterface for the psr

--- Controller/ControllerInterface.php
+++ Controller/ControllerInterface.php
@@ -6,44 +6,40 @@
 
 namespace Pam\Controller;
 
-
 /** ***********************************\
-* All controller classes specifications
-*/
+ * All controller classes specifications
+ */
 interface ControllerInterface
 {
+    /** ***********************\
+     * Returns the url of a page
+     * @param  string $page   => the name of the page
+     * @param  array  $params => the url parameters
+     * @return string         => the page url
+     */
+    public function url(string $page, array $params = []);
 
-  /** ***********************\
-  * Returns the url of a page
-  * @param  string $page   => the name of the page
-  * @param  array  $params => the url parameters
-  * @return string         => the page url
-  */
-  public function url(string $page, array $params = []);
+    /** *****************************************\
+     * Redirects to a page with the function url()
+     * @param string $page   => the name of the page
+     * @param array  $params => the url parameters
+     */
+    public function redirect(string $page, array $params = []);
 
+    /** ***************\
+     * Renders the views
+     * @param  string $view   => the view to render
+     * @param  array  $params => (the parameters to render the view)
+     * @return string         => the render of the view
+     */
+    public function render(string $view, array $params = []);
 
-  /** *****************************************\
-  * Redirects to a page with the function url()
-  * @param string $page   => the name of the page
-  * @param array  $params => the url parameters
-  */
-  public function redirect(string $page, array $params = []);
-
-
-  /** ***************\
-  * Renders the views
-  * @param  string $view   => the view to render
-  * @param  array  $params => (the parameters to render the view)
-  * @return string         => the render of the view
-  */
-  public function render(string $view, array $params = []);
-
-
-  /** *********************************\
-  * Uploads a file to a specific folder
-  * And returns the file name
-  * @param  string $fileDir  => the folder(s) from the file folder
-  * @return string $fileName => the file name
-  */
-  public function upload($fileDir);
+    /** *********************************\
+     * Uploads a file to a specific folder
+     * And returns the file name
+     * @param  string $fileDir  => the folder(s) from the file folder
+     * @return string $fileName => the file name
+     */
+    public function upload($fileDir);
 }
+
