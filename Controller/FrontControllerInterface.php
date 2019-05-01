397f2372963d25ad8604cd2dc4cb56b4373810b4
Philippe Beck <philippe@philippebeck.net>
2019-05-01 16:03:59 +0200

    Fix the FrontControllerInterface for the psr

--- Controller/FrontControllerInterface.php
+++ Controller/FrontControllerInterface.php
@@ -6,44 +6,39 @@
 
 namespace Pam\Controller;
 
-
 /** ************************************\
  * Front controller class specifications
  */
 interface FrontControllerInterface
 {
+    /** *************************\
+     * Creates the template loader
+     * Then the template engine & adds it extensions
+     */
+    public function setTemplate();
 
-  /** *************************\
-  * Creates the template loader
-  * Then the template engine & adds it extensions
-  */
-  public function setTemplate();
+    /** **********************************************\
+     * Checks if the request query is present, cuts it
+     * And gives each part to the current controller & the current action method
+     */
+    public function parseUrl();
 
+    /** **************************\
+     * Sets the current controller
+     * Checks if exists & sets it as default if necessary
+     */
+    public function setController();
 
-  /** **********************************************\
-   * Checks if the request query is present, cuts it
-   * And gives each part to the current controller & the current action method
-   */
-  public function parseUrl();
+    /** *****************************\
+     * Sets the current action method
+     * Checks if exists & sets it as default if necessary
+     */
+    public function setAction();
 
-
-  /** **************************\
-   * Sets the current controller
-   * Checks if exists & sets it as default if necessary
-   */
-  public function setController();
-
-
-  /** *****************************\
-   * Sets the current action method
-   * Checks if exists & sets it as default if necessary
-   */
-  public function setAction();
-
-
-  /** ******************************\
-   * Creates the controller instance
-   * Then call the action method on it
-   */
-  public function run();
+    /** ******************************\
+     * Creates the controller instance
+     * Then call the action method on it
+     */
+    public function run();
 }
+
