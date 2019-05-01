306169f02bdb9984a5917daa9a27596c58dd104b
Philippe Beck <philippe@philippebeck.net>
2019-05-01 16:04:44 +0200

    Fix the DatabaseInterface for the psr

--- Database/DatabaseInterface.php
+++ Database/DatabaseInterface.php
@@ -6,36 +6,33 @@
 
 namespace Pam\Database;
 
-
 /** *********************************\
-* All database classes specifications
-*/
+ * All database classes specifications
+ */
 interface DatabaseInterface
 {
+    /** *********************\
+     * Returns a unique result
+     * @param  string $query  => the database query
+     * @param  array  $params => (the query parameters)
+     * @return mixed          => the result
+     */
+    public function result(string $query, array $params = []);
 
-  /** *********************\
-  * Returns a unique result
-  * @param  string $query  => the database query
-  * @param  array  $params => (the query parameters)
-  * @return mixed          => the result
-  */
-  public function result(string $query, array $params = []);
+    /** *****************\
+     * Returns all results
+     * @param  string $query  => the database query
+     * @param  array  $params => (the query parameters)
+     * @return mixed          => the results
+     */
+    public function results(string $query, array $params = []);
 
-
-  /** *****************\
-  * Returns all results
-  * @param  string $query  => the database query
-  * @param  array  $params => (the query parameters)
-  * @return mixed          => the results
-  */
-  public function results(string $query, array $params = []);
-
-
-  /** ****************\
-  * Executes an action
-  * @param  string $query  => the database query
-  * @param  array  $params => (the query parameters)
-  * @return mixed          => the boolean value
-  */
-  public function action(string $query, array $params = []);
+    /** ****************\
+     * Executes an action
+     * @param  string $query  => the database query
+     * @param  array  $params => (the query parameters)
+     * @return mixed          => the boolean value
+     */
+    public function action(string $query, array $params = []);
 }
+
