fc5fabbe98bc82bcabcb23f9a4c55ee621a2c4c9
Philippe Beck <philippe@philippebeck.net>
2019-05-01 16:06:46 +0200

    Fix the PDOFactory for the psr

--- Database/PDOFactory.php
+++ Database/PDOFactory.php
@@ -8,52 +8,51 @@ namespace Pam\Database;
 
 use \PDO;
 
-
 /** *********************************************\
  * Creates the pdo connection if it doesn't exist
  */
 class PDOFactory
 {
-  // Unique pdo instance starts at null
-  static private $pdo = null;
+    // Unique pdo instance starts at null
+    static private $pdo = null;
 
-
-  /** *********************************\
-  * Returns the connection if it exists
-  * Otherwise it creates it before returning it
-  * @return PDO $pdo => The connection to the database
-  */
-  public static function getConnection()
-  {
-    // DataBase constants
-    require_once dirname(dirname(dirname(dirname(__DIR__)))).'/config/bdd.php';
-
-    // Checks if Pdo connection doesn't exist
-    if (is_null(self::$pdo))
+    /** *********************************\
+     * Returns the connection if it exists
+     * Otherwise it creates it before returning it
+     * @return PDO $pdo => The connection to the database
+     */
+    public static function getConnection()
     {
-      // Creates the first parameters for the new pdo concerning the database
-      $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+        // DataBase constants
+        require_once dirname(dirname(dirname(dirname(__DIR__)))).'/config/bdd.php';
 
-      // Sets & stores the pdo options as the last parameters for the new pdo
-      $options = [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      ];
-      // Creates & stores the pdo connection
-      $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
+        // Checks if Pdo connection doesn't exist
+        if (is_null(self::$pdo))
+        {
+            // Creates the first parameters for the new pdo concerning the database
+            $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
 
-      // Sets the encoding characters
-      $pdo->exec('SET NAMES UTF8');
+            // Sets & stores the pdo options as the last parameters for the new pdo
+            $options = [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ];
+            // Creates & stores the pdo connection
+            $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
 
-      // Stores the pdo connection
-      self::$pdo = $pdo;
+            // Sets the encoding characters
+            $pdo->exec('SET NAMES UTF8');
 
-      // Returns the connection
-      return $pdo;
+            // Stores the pdo connection
+            self::$pdo = $pdo;
+
+            // Returns the connection
+            return $pdo;
+        }
+        else {
+            // Return the pdo connection
+            return self::$pdo;
+        }
     }
-    else {
-      // Return the pdo connection
-      return self::$pdo;
-    }
-  }
 }
+
