5fb17c676ec06883efcd9f46135445eab124df4f
Philippe Beck <philippe@philippebeck.net>
2019-05-14 19:14:35 +0200

    DatabaseInterface : remove all useless comments & PHPDocs

--- Database/DatabaseInterface.php
+++ Database/DatabaseInterface.php
@@ -1,37 +1,31 @@
 <?php
 
-// ****************************** \\
-// ***** DATABASE INTERFACE ***** \\
-// ****************************** \\
-
 namespace Pam\Database;
 
-/** *********************************\
- * All database classes specifications
+/**
+ * Interface DatabaseInterface
+ * @package Pam\Database
  */
 interface DatabaseInterface
 {
-    /** *********************\
-     * Returns a unique result
-     * @param  string $query  => the database query
-     * @param  array  $params => (the query parameters)
-     * @return mixed          => the result
+    /**
+     * @param string $query
+     * @param array $params
+     * @return mixed
      */
     public function result(string $query, array $params = []);
 
-    /** *****************\
-     * Returns all results
-     * @param  string $query  => the database query
-     * @param  array  $params => (the query parameters)
-     * @return mixed          => the results
+    /**
+     * @param string $query
+     * @param array $params
+     * @return mixed
      */
     public function results(string $query, array $params = []);
 
-    /** ****************\
-     * Executes an action
-     * @param  string $query  => the database query
-     * @param  array  $params => (the query parameters)
-     * @return mixed          => the boolean value
+    /**
+     * @param string $query
+     * @param array $params
+     * @return mixed
      */
     public function action(string $query, array $params = []);
 }
