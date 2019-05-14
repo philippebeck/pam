5fc10f94ca723a1692e24b222e53f86a28cb9418
Philippe Beck <philippe@philippebeck.net>
2019-05-14 19:22:03 +0200

    Controller : remove useless comments, PHPDocs, add @throws & twig use

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,100 +1,87 @@
 <?php
 
-// ********************** \\
-// ***** CONTROLLER ***** \\
-// ********************** \\
-
 namespace Pam\Controller;
 
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 use Pam\Helper\Session;
 
-/** **********************\
- * Generic controller class
+/**
+ * Class Controller
+ * @package Pam\Controller
  */
 abstract class Controller implements ControllerInterface
 {
-    // The Twig engine
+    /**
+     * @var Environment
+     */
     private $twig;
 
-    /** **********************************\
-     * Receives the Twig engine & stores it
-     * @param Twig_Environment $twig => the Twig engine
+    /**
+     * Controller constructor
+     * @param Environment $twig
      */
-    public function __construct(\Twig_Environment $twig)
+    public function __construct(Environment $twig)
     {
-        // Stores the Twig engine
         $this->twig = $twig;
     }
 
-    /** ***********************\
-     * Returns the url of a page
-     * @param  string $page   => the name of the page
-     * @param  array  $params => the url parameters
-     * @return string         => the page url
+    /**
+     * @param string $page
+     * @param array $params
+     * @return string
      */
     public function url(string $page, array $params = [])
     {
-        // Includes the page in the params array with the key 'access'
         $params['access'] = $page;
 
-        // Returns the generate url
         return 'index.php?' . http_build_query($params);
     }
 
-    /** *****************************************\
-     * Redirects to a page with the function url()
-     * @param string $page   => the name of the page
-     * @param array  $params => the url parameters
+    /**
+     * @param string $page
+     * @param array $params
      */
     public function redirect(string $page, array $params = [])
     {
-        // Redirects with the url function
         header('Location: ' . $this->url($page, $params));
         exit;
     }
 
-    /** ***************\
-     * Renders the views
-     * @param  string $view   => the view to render
-     * @param  array  $params => (the parameters to render the view)
-     * @return string         => the render of the view
+    /**
+     * @param string $view
+     * @param array $params
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function render(string $view, array $params = [])
     {
-        // Returns the rendering of the view
         return $this->twig->render($view, $params);
     }
 
-    /** *********************************\
-     * Uploads a file to a specific folder
-     * And returns the file name
-     * @param  string $fileDir  => the folder(s) from the file folder
-     * @return string $fileName => the file name
+    /**
+     * @param string $fileDir
+     * @return string
      */
     public function upload($fileDir)
     {
-        // Checks if there is an upload error
         if ($_FILES['file']['error'] > 0) {
-
-            // Creates a warning message to inform the user about the error
             htmlspecialchars(Session::createAlert('Erreur lors du transfert du fichier...', 'warning'));
         } else {
-            // Stores the file name
             $fileName = $_FILES['file']['name'];
 
-            // Stores the destination path of the file
             $filePath = dirname(dirname(dirname(dirname(__DIR__)))) . "/public/{$fileDir}/{$fileName}";
 
-            // Moves the new file to his folder
             $result  = move_uploaded_file($_FILES['file']['tmp_name'], $filePath);
 
-            // Checks if there is a result
             if ($result) {
-
-                // Creates a valid message to confirm the transfer
                 htmlspecialchars(Session::createAlert('Transfert du nouveau fichier réussi !', 'valid'));
             }
-            // Returns the file name
+
             return $fileName;
         }
     }
