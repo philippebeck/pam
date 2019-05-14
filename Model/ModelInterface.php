76164a42aa0c74882aea0b475b4585acfcdcd7f0
Philippe Beck <philippe@philippebeck.net>
2019-05-14 19:18:03 +0200

    ModelInterface : remove all useless comments & PHPDocs

--- Model/ModelInterface.php
+++ Model/ModelInterface.php
@@ -1,55 +1,43 @@
 <?php
 
-// *************************** \\
-// ***** MODEL INTERFACE ***** \\
-// *************************** \\
-
 namespace Pam\Model;
 
-/** ******************************\
- * All model classes specifications
+/**
+ * Interface ModelInterface
+ * @package Pam\Model
  */
 interface ModelInterface
 {
-    /** ***************************************************\
-     * Lists all objects with or without the order by clause
-     * Or lists objects from a key with or without the order by clause
-     * @param  string  $value => (the name of the where clause value)
-     * @param  string  $key   => (the name of the where clause key)
-     * @param  int     $order => (the order by clause with ASC(1) or DESC)
-     * @return array          => the results of the query
+    /**
+     * @param string|null $value
+     * @param string|null $key
+     * @param int $order
+     * @return array|mixed
      */
     public function list(string $value = null, string $key = null, int $order = 0);
 
-    /** ******************\
-     * Creates a new object
-     * @param array $data => the data of the new object
+    /**
+     * @param array $data
      */
     public function create(array $data);
 
-    /** *************************\
-     * Reads an object from his id
-     * Or from another key
-     * @param  string $value => the name of the where clause value
-     * @param  string $key   => (the name of the where clause key)
-     * @return array         => the results of the query
+    /**
+     * @param string $value
+     * @param string|null $key
+     * @return array|mixed
      */
     public function read(string $value, string $key = null);
 
-    /** ***************************\
-     * Updates an object from his id
-     * Or from another key
-     * @param string $value => the name of the where clause value
-     * @param array  $data  => the data to update the object
-     * @param string $key   => (the name of the where clause key)
+    /**
+     * @param string $value
+     * @param array $data
+     * @param string|null $key
      */
     public function update(string $value, array $data, string $key = null);
 
-    /** ***************************\
-     * Deletes an object from his id
-     * Or from another key
-     * @param  string $value => the name of the where clause value
-     * @param  string $key   => (the name of the where clause key)
+    /**
+     * @param string $value
+     * @param string|null $key
      */
     public function delete(string $value, string $key = null);
 }
