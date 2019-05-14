1dcda9a8e4f566af0ee2601e43801e454f251b59
Philippe Beck <philippe@philippebeck.net>
2019-05-14 19:17:26 +0200

    ModelFactory : remove all useless comments & PHPDocs

--- Model/ModelFactory.php
+++ Model/ModelFactory.php
@@ -1,52 +1,39 @@
 <?php
 
-// ************************* \\
-// ***** MODEL FACTORY ***** \\
-// ************************* \\
-
 namespace Pam\Model;
 
 use Pam\Database\PDODatabase;
 use Pam\Database\PDOFactory;
 
-/** ************************************\
- * Creates the model if it doesn't exist
+/**
+ * Class ModelFactory
+ * @package Pam\Model
  */
 class ModelFactory
 {
-    // The empty model
+    /**
+     * @var array
+     */
     static private $models = [];
 
-    /** ****************************\
-     * Returns the model if it exists
-     * Otherwise it creates the connection & the model before returning it
-     * @param  string $table => the table name of the active model
-     * @return Model  $model => the object of the model concerned
+    /**
+     * @param $table
+     * @return mixed
      */
     static public function get($table)
     {
-        // Checks if object model exist
         if (array_key_exists($table, self::$models)) {
-
-            // Returns the existing model object
             return self::$models[$table];
         }
-        // Assembling the class name with the appropriate namespace
-        $class = 'App\Model\\' . ucfirst($table) . 'Model';
 
-        // Creates the pdo connection
-        $pdo = PDOFactory::getConnection();
+        $class  = 'App\Model\\' . ucfirst($table) . 'Model';
+        $pdo    = PDOFactory::getConnection();
 
-        // Creates the database object
-        $database = new PDODatabase($pdo);
+        $database   = new PDODatabase($pdo);
+        $model      = new $class($database);
 
-        // Creates the model object
-        $model = new $class($database);
-
-        // Saves the model object
         self::$models[$table] = $model;
 
-        // Returns the model object
         return $model;
     }
 }
