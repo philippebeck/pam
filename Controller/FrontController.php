f5fcd8e2b30568cef814db439234cb050190ac28
Philippe Beck <philippe@philippebeck.net>
2019-05-14 19:52:33 +0200

    FrontController : remove useless comments & PHPDocs

--- Controller/FrontController.php
+++ Controller/FrontController.php
@@ -1,148 +1,119 @@
 <?php
 
-// **************************** \\
-// ***** FRONT CONTROLLER ***** \\
-// **************************** \\
-
 namespace Pam\Controller;
 
-use \Twig\Environment;
-use \Twig\Extension\DebugExtension;
-use \Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
 use Pam\View\PamTwigExtension;
 
-/** ****************************\
- * Generic front controller class
+/**
+ * Class FrontController
+ * @package Pam\Controller
  */
 class FrontController implements FrontControllerInterface
 {
-    // Default constants
     const DEFAULT_PATH        = 'App\Controller\\';
     const DEFAULT_CONTROLLER  = 'HomeController';
     const DEFAULT_ACTION      = 'IndexAction';
 
-    // Protected properties
-    protected $twig           = null;
-    protected $page           = null;
-    protected $controller     = self::DEFAULT_CONTROLLER;
-    protected $action         = self::DEFAULT_ACTION;
+    /**
+     * @var null
+     */
+    protected $twig = null;
 
-    /** *****************************\
-     * Launchs the session, then sets the template engine
-     * Parse the url, then sets the controller & the action method
+    /**
+     * @var null
+     */
+    protected $page = null;
+
+    /**
+     * @var string
+     */
+    protected $controller = self::DEFAULT_CONTROLLER;
+
+    /**
+     * @var string
+     */
+    protected $action = self::DEFAULT_ACTION;
+
+    /**
+     * FrontController constructor
      */
     public function __construct()
     {
-        // Sets the template engine
         $this->setTemplate();
-
-        // Parses the request query
         $this->parseUrl();
-
-        // Sets the controller
         $this->setController();
-
-        // Sets the action method
         $this->setAction();
     }
 
-    /** *************************\
-     * Creates the template loader
-     * Then the template engine & adds its extensions
+    /**
+     * @return mixed|void
      */
     public function setTemplate()
     {
-        // Creates the loader & sets the template directory path
         $loader = new FilesystemLoader('../src/View');
-
-        // Creates the template engine
-        $twig = new Environment($loader, array(
+        $twig   = new Environment($loader, array(
             'cache' => false,
             'debug' => true
         ));
 
-        // Adds Twig extensions
         $twig->addExtension(new DebugExtension());
         $twig->addExtension(new PamTwigExtension());
 
-        // Attributes the template engine to the current object
         $this->twig = $twig;
     }
 
-    /** **********************************************\
-     * Checks if the request query is present, cuts it
-     * And gives each part to the current controller & the current action method
+    /**
+     * @return mixed|void
      */
     public function parseUrl()
     {
-        // Checks if the key access doesn't exist or if this key is not define
         if (!array_key_exists('access', $_GET) || !isset($_GET['access'])) {
             $_GET['access'] = 'home';
         }
-        // Stores the $_GET['access'] value to this page
-        $this->page = $_GET['access'];
 
-        // Cuts this page value with the exclamation point
+        $this->page = $_GET['access'];
         $access = explode('!', $this->page);
 
-        // Attributes the first access string to this controller
-        $this->controller = $access[0];
-
-        // If set, attributes the second access string to the current action method
-        // If not set, attributes the string index
-        $this->action = count($access) == 1 ? 'index' : $access[1];
+        $this->controller   = $access[0];
+        $this->action       = count($access) == 1 ? 'index' : $access[1];
     }
 
-    /** **************************\
-     * Sets the current controller
-     * Checks if exists & sets it as default if necessary
+    /**
+     * @return mixed|void
      */
     public function setController()
     {
-        // Constructs the current controller
         $this->controller = ucfirst(strtolower($this->controller)) . 'Controller';
-
-        // Constructs the current controller with the default path
         $this->controller = self::DEFAULT_PATH . $this->controller;
 
-        // Checks if the current controller is an existing class
         if (!class_exists($this->controller)) {
-            
-            // Attributes the default path & controller to the current controller
             $this->controller = self::DEFAULT_PATH . self::DEFAULT_CONTROLLER;
         }
     }
 
-    /** *****************************\
-     * Sets the current action method
-     * Checks if exists & sets it as default if necessary
+    /**
+     * @return mixed|void
      */
     public function setAction()
     {
-        // Constructs the current action method
         $this->action = strtolower($this->action) . 'Action';
 
-        // Checks if the current action method exists in the current controller
         if (!method_exists($this->controller, $this->action)) {
-
-            // Attributes the default action method to the current action method
             $this->action = self::DEFAULT_ACTION;
         }
     }
 
-    /** ******************************\
-     * Creates the controller instance
-     * Then call the action method on it
+    /**
+     * @return mixed|void
      */
     public function run()
     {
-        // Creates the current controller instance
-        $this->controller = new $this->controller($this->twig);
+        $this->controller   = new $this->controller($this->twig);
+        $response           = call_user_func([$this->controller, $this->action]);
 
-        // Calls the action method on the controller
-        $response = call_user_func([$this->controller, $this->action]);
-
-        // Shows the response
         echo $response;
     }
 }
