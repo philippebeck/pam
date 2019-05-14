4d88f290786b2002b0aa057b843a98960575b7f6
Philippe Beck <philippe@philippebeck.net>
2019-05-14 20:14:43 +0200

    Remove useless comments & add comments to replace db name

--- Project/config/pam.sql
+++ Project/config/pam.sql
@@ -1,20 +1,13 @@
--- ************************
--- ***** DATABASE PAM *****
--- ************************
-
-
-
 -- !! This is a database example, allowing to start the Framework if needed...
 
-
-
 -- Drops the old database before creates a new one
+-- You need to replace pam with your db name
 DROP DATABASE IF EXISTS pam;
 
-
 -- Creates the database
-CREATE DATABASE pam CHARACTER SET 'utf8';
-
+-- You need to replace pam with your db name
+CREATE DATABASE pam CHARACTER SET utf8;
 
 -- Uses the database
+-- You need to replace pam with your db name
 USE pam;
