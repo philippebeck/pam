5106e530dc9e2b14fe42b70d59b7ad42dacd889f
Philippe Beck <philippe@philippebeck.net>
2019-05-14 19:51:35 +0200

    FrontControllerInterface : remove useless PHPDocs

--- Controller/FrontControllerInterface.php
+++ Controller/FrontControllerInterface.php
@@ -1,43 +1,35 @@
 <?php
 
-// ************************************** \\
-// ***** FRONT CONTROLLER INTERFACE ***** \\
-// ************************************** \\
-
 namespace Pam\Controller;
 
-/** ************************************\
- * Front controller class specifications
+/**
+ * Interface FrontControllerInterface
+ * @package Pam\Controller
  */
 interface FrontControllerInterface
 {
-    /** *************************\
-     * Creates the template loader
-     * Then the template engine & adds it extensions
+    /**
+     * @return mixed|void
      */
     public function setTemplate();
 
-    /** **********************************************\
-     * Checks if the request query is present, cuts it
-     * And gives each part to the current controller & the current action method
+    /**
+     * @return mixed|void
      */
     public function parseUrl();
 
-    /** **************************\
-     * Sets the current controller
-     * Checks if exists & sets it as default if necessary
+    /**
+     * @return mixed|void
      */
     public function setController();
 
-    /** *****************************\
-     * Sets the current action method
-     * Checks if exists & sets it as default if necessary
+    /**
+     * @return mixed|void
      */
     public function setAction();
 
-    /** ******************************\
-     * Creates the controller instance
-     * Then call the action method on it
+    /**
+     * @return mixed|void
      */
     public function run();
 }
