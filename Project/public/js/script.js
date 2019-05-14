743f65973a0041aff132d98b02d36b6eb388f327
Philippe Beck <philippe@philippebeck.net>
2019-05-14 20:15:19 +0200

    Remove useless comments from the script

--- Project/public/js/script.js
+++ Project/public/js/script.js
@@ -1,18 +1,5 @@
-// ****************** \\
-// ***** SCRIPT ***** \\
-// ****************** \\
-
-
-
-// Strict mode
 'use strict';
 
-
-
-// Waits the complete page loading to launch the script
-document.addEventListener('DOMContentLoaded', function(e)
-{
+document.addEventListener('DOMContentLoaded', function(e) {
   // Put your project script here
-
-  }
-);
+});
