cf5100566380aafca1b96d9c4d7ea0d7ebb9af5b
Philippe Beck <philippe@philippebeck.net>
2019-05-14 20:15:44 +0200

    Remove useless comments & add session to index

--- Project/public/index.php
+++ Project/public/index.php
@@ -1,26 +1,17 @@
 <?php
 
-// ***************** \\
-// ***** INDEX ***** \\
-// ***************** \\
-
 use Pam\Controller\FrontController;
 use Pam\Helper\Session;
 use Tracy\Debugger;
 
-// Loads Composer autoload
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-// Starts or continues session
-$session = new Session();
-
-// Creates a front controller instance
-$frontController = new FrontController();
+$session            = new Session();
+$frontController    = new FrontController();
 
 // Basic tests area
 Debugger::enable();
 // print_r($_SESSION);
 // var_dump($frontController);
 
-// Calls the run method on the front controller instance
 $frontController->run();
