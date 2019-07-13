6f3d51f711e4f4815ed537222ec1e7b1063f6285
Philippe Beck <philippe@philippebeck.net>
2019-07-13 18:30:29 +0200

    Fix upload method from Controller

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,7 +2,7 @@
 
 namespace Pam\Controller;
 
-use http\Exception\RuntimeException;
+use Exception;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -75,48 +75,47 @@ abstract class Controller implements ControllerInterface
     }
 
     /**
-     * @param string $fileDir
-     * @return string
+     * @param $fileDir
+     * @return mixed
      */
     public function upload($fileDir)
     {
-        $file = filter_var_array($_FILES['file'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $file           = filter_var_array($_FILES['file']);
+        $destination    = "__DIR__ . {$fileDir}/{$file['name']}";
 
         try {
             if (!isset($file['error']) || is_array($file['error'])) {
-                throw new RuntimeException('Invalid parameters...');
+                throw new Exception('Invalid parameters...');
             }
 
             switch ($file['error']) {
                 case UPLOAD_ERR_OK:
                     break;
                 case UPLOAD_ERR_NO_FILE:
-                    throw new RuntimeException('No file sent...');
+                    throw new Exception('No file sent...');
                 case UPLOAD_ERR_INI_SIZE:
                 case UPLOAD_ERR_FORM_SIZE:
-                    throw new RuntimeException('Exceeded filesize limit...');
+                    throw new Exception('Exceeded filesize limit...');
                 default:
-                    throw new RuntimeException('Unknown errors...');
+                    throw new Exception('Unknown errors...');
             }
 
             if ($file['size'] > 1000000) {
-                throw new RuntimeException('Exceeded filesize limit...');
+                throw new Exception('Exceeded filesize limit...');
             }
 
-            if (!move_uploaded_file(filter_var($_FILES['file']['tmp_name']), "{$fileDir}/{$file['name']}")) {
-                $this->cookie->createAlert('Failed to move uploaded file...');
-                //throw new RuntimeException('Failed to move uploaded file...');
+            if (!move_uploaded_file(filter_var($file['tmp_name']), $destination)) {
+                throw new Exception('Failed to move uploaded file...');
             }
 
             $this->cookie->createAlert('File is uploaded successfully !');
 
             return $file['name'];
 
-        } catch (RuntimeException $e) {
+        } catch (Exception $e) {
 
             $this->cookie->createAlert($e->getMessage());
         }
     }
 }
-
 
