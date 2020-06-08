612c0358e114107556b2906087853777293e745d
Philippe Beck <philippe@philippebeck.net>
2020-06-08 16:03:00 +0200

    Update uploadFile() to add $fileName @param & $fileExt switch

--- core/Controller/Globals/FilesController.php
+++ core/Controller/Globals/FilesController.php
@@ -58,25 +58,45 @@ class FilesController
     }
 
     /**
-     * @param $fileDir
-     * @return mixed
+     * @param string $fileDir
+     * @param string|null $fileName
+     * @return mixed|string
      */
-    public function uploadFile(string $fileDir)
+    public function uploadFile(string $fileDir, string $fileName = null)
     {
+        if ($fileName === null) {
+            $dest = $fileDir . $this->file["name"];
+        } else {
+            switch ($this->file["type"]) {
+                case "image/jpeg":
+                    $fileExt = ".jpg";
+                    break;
+                case "image/png":
+                    $fileExt = ".png";
+                    break;
+                case "image/gif":
+                    $fileExt = ".gif";
+                    break;
+                default:
+                    throw new Exception("Image Type not accepted...");
+            }
+            $dest = $fileDir . $fileName . $fileExt;
+        }
+
         try {
-            if (!isset($this->file['error']) || is_array($this->file['error'])) {
-                throw new Exception('Invalid parameters...');
+            if (!isset($this->file["error"]) || is_array($this->file["error"])) {
+                throw new Exception("Invalid parameters...");
             }
 
-            if ($this->file['size'] > 1000000) {
-                throw new Exception('Exceeded filesize limit...');
+            if ($this->file["size"] > 1000000) {
+                throw new Exception("Exceeded filesize limit...");
             }
 
-            if (!move_uploaded_file($this->file['tmp_name'], "{$fileDir}/{$this->file['name']}")) {
-                throw new Exception('Failed to move uploaded file...');
+            if (!move_uploaded_file($this->file["tmp_name"], $dest)) {
+                throw new Exception("Failed to move uploaded file...");
             }
 
-            return $this->file['name'];
+            return $this->file["name"];
 
         } catch (Exception $e) {
 
