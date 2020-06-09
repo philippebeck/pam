413e7eb16964052f37813d1fd9a018505971aed0
Philippe Beck <philippe@philippebeck.net>
2020-06-09 06:57:37 +0200

    Create cleanString method to MainController

--- core/Controller/MainController.php
+++ core/Controller/MainController.php
@@ -50,6 +50,40 @@ abstract class MainController
     }
 
     /**
+     * @param string $string
+     * @param bool $isLow
+     * @return string|string[]|null
+     */
+    public function cleanString(string $string, bool $isLow = true)
+    {
+        $string =
+            str_replace(" ", "-",
+                str_replace(array("ù", "û", "ü"), "u",
+                    str_replace(array("ô", "ö"), "o",
+                        str_replace(array("î", "ï"), "i",
+                            str_replace(array("é", "è", "ê", "ë"), "e",
+                                str_replace(array("ç"), "c",
+                                    str_replace(array("à", "â", "ä"), "a", $string)
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+
+        $string =
+            preg_replace("/-+/", "-",
+                preg_replace("/[^A-Za-z0-9\-]/", "", $string)
+            );
+
+        if ($isLow === true) {
+            $string = strtolower($string);
+        }
+
+        return $string;
+    }
+
+    /**
      * @param string $page
      * @param array $params
      * @return string
